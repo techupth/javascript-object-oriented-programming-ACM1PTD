03d2c4c72c45fa9c13786e453d3d8703985e68cb
Nonthawath Patchakapati <npc2540@hotmail.com>
2024-04-26 16:14:36 +0700

    adding answers

--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,29 @@
-class Notification { }
+class Notification {
+    constructor(notificationId, createdTime, content, receiver) {
+      this.notificationId = notificationId;
+      this.createdTime = createdTime;
+      this.content = content;
+      this.receiver = receiver;
+    }
+    send() {
+      console.log(`Notification has been sent to ${this.receiver}`);
+    }
+}
+  
+class EmailNotification extends Notification {
+    constructor(notificationId, createdTime, content, receiver) {
+      super(notificationId, createdTime, content, receiver);
+    }
+}
+  
+class SMSNotification extends Notification {
+    constructor(notificationId, createdTime, content, receiver) {
+      super(notificationId, createdTime, content, receiver);
+    }
+}
 
-class EmailNotification { }
+const johnEmail = new EmailNotification("123", "10.00", "ABC", "john123@gmail.com")
+const johnNumber = new SMSNotification("456", "12.00", "DEF", "0987654321")
 
-class SMSNotification { }
+johnEmail.send()
+johnNumber.send()
